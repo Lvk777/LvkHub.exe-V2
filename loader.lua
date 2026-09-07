3185d910dc603b93eb7d6eb01418706b70c1d3f2
Lvk777 <157662022+Lvk777@users.noreply.github.com>
2026-09-07 19:22:46 -0300

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -16,7 +16,7 @@ shared.LvkHubStartupHidden = true
 
 local RunService = game:GetService("RunService")
 local CoreGui = game:GetService("CoreGui")
-local BASE = "https://raw.githubusercontent.com/Lvk777/LvkHub.exe/main/"
+local BASE = "https://raw.githubusercontent.com/Lvk777/LvkHub.exe-V2/main/"
 
 local function loadModule(path)
     local src = game:HttpGet(BASE .. path, true)
