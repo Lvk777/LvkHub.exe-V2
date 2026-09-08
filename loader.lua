297cb37f4f12290ac4799b5a4bf23c618e08b2d8
Lvk777 <157662022+Lvk777@users.noreply.github.com>
2026-09-08 07:08:20 -0300

    feat(loader): load runtime health monitor

--- loader.lua
+++ loader.lua
@@ -145,6 +145,7 @@ local ok, err = pcall(function()
     loadModule("src/Utility/Presets.lua")(State, Registry, UI)
     loadModule("src/Utility/SoloSurvivalV2.lua")(State, Registry, UI)
     loadModule("src/Utility/ServerStateMonitorV1.lua")(State, Registry, UI)
+    loadModule("src/Utility/RuntimeHealthMonitorV1.lua")(State, Registry, TargetProvider, UI)
     loadModule("src/World/Main.lua")(State, Registry, UI)
 
     loadModule("src/Local/MainV4.lua")(State, Registry, UI)
